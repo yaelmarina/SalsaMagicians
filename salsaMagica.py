6bb309276a797f447d1406c000907cc3dfe5b33f
yaelmarina <47425354+yaelmarina@users.noreply.github.com>
2020-08-17 19:42:33 -0400

    Create salsaMagica.py
    Esqueleto de aplicación

---
+++ salsaMagica.py
@@ -0,0 +1,10 @@
+#Esta app servirá para que los magos de Harry Potter toquen Salsa
+characterOne = "Snape"
+characterTwo = "Hermione"
+characterThree = "Hagrid"
+
+#CharacterOne
+if (characterOne == "Snape"):
+  print("Snape toca los bongós")
+elif (characterOne == "Harry"):
+  print("Harry toca la trompeta")
